e656dd20f68a75a2aec3fdf3dd1edf28c5b00447
Joe Sanjo <57857214+R3tr0gh057@users.noreply.github.com>
2022-01-31 16:50:42 +0530

    more moderation commands
    -unban
    -mute
    -unmute
    exception handling included

--- dedsec.py
+++ dedsec.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 from datetime import datetime
+from discord.ext.commands import has_permissions, MissingPermissions
 import time
 import random
 
@@ -27,9 +28,23 @@ async def on_ready():
     #print(f'Guild Members:\n - {members}')
     print(", ".join([member.name for member in guild.members]))
 
-async def on_message_delete(self, message):
-        msg = f'{message.author} has deleted the message: {message.content}'
-        await message.channel.send(msg)
+#mention's if someone deletes a message (4k)
+@client.event
+async def on_message_delete(ctx):
+        msg = f'{ctx.author} has deleted the message: {ctx.content}'
+        await ctx.channel.send(msg)
+
+#potato flagger
+'''flag=['potat','potato']
+@client.event
+async def on_message(ctx):
+    msg=ctx.content
+    lower=msg.lower()
+    for word in flag:
+        if word in lower:
+            await ctx.delete()
+            await ctx.channel.send('haha no')
+    await client.process_commands()'''
 
 @client.command(name='ping', help='Returns the client latency.')
 async def ping(ctx):
@@ -45,11 +60,8 @@ async def ban(ctx, member : discord.Member,*,reason="No reason provided"):
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-    try:
-        await ctx.channel.send(member.display_name+" has been banned from "+guild.name)
-        await member.ban(reason=reason)
-    except:
-        ctx.channel.send('You dont have the perms to do that')
+    await ctx.channel.send(member.display_name+" has been banned from "+guild.name)
+    await member.ban(reason=reason)
     try:
         await member.send("You have been banned from "+guild.name+" for "+reason)
     except:
@@ -61,14 +73,75 @@ async def kick(ctx, member : discord.Member,*,reason="No reason provided"):
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-    try:
-        await ctx.channel.send(member.display_name+" has been kicked from "+guild.name)
-        await member.kick(reason=reason)
-    except:
-        ctx.channel.send('You dont have the perms to do that')
+    await ctx.channel.send(member.display_name+" has been kicked from "+guild.name)
+    await member.kick(reason=reason)
     try:
         await member.send("You have been kicked from "+guild.name+" for "+reason)
     except:
         print(member.display_name+" is not accepting dms")
+
+@client.command(name='unban', help='unban a banned member')
+@commands.has_permissions(ban_members = True)
+async def unban(ctx,*,member):
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    banned_users= await ctx.guild.bans()
+    member_name, member_disc =member.split('#')
+    for banned_entry in banned_users:
+        user=banned_entry.user
+        if (user.name, user.discriminator)==(member_name, member_disc):
+            await ctx.guild.unban(user)
+            await ctx.channel.send(member_name+" has been unbanned")
+            return
+    await ctx.send(member+" is not in the ban list")
+    
+    try:
+        await member.send("You have been unbanned in "+guild.name)
+    except:
+        print(member.display_name+" is not accepting dms")
+
+@client.command(name='mute', help='mute a member')
+@commands.has_permissions(kick_members = True)
+async def mute(ctx, member : discord.Member):
+    muted_role= ctx.guild.get_role(937655146182246401)
+    await member.add_roles(muted_role)
+    await ctx.channel.send(member.mention+' has been muted')
+
+@client.command(name='unmute', help='unmute a member')
+@commands.has_permissions(kick_members = True)
+async def unmute(ctx, member : discord.Member):
+    muted_role= ctx.guild.get_role(937655146182246401)
+    await member.remove_roles(muted_role)
+    await ctx.channel.send(member.mention+', your mute has been lifted')
+
+#exception handling start
+
+@ban.error
+async def ban_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        await ctx.message.send("You dont have the perms to ban members")
+
+@kick.error
+async def kick_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        await ctx.message.send("You dont have the perms to kick members")
+
+@unban.error
+async def unban_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        await ctx.message.send("You dont have the perms to unban members")
+
+@mute.error
+async def mute_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        await ctx.message.send("You dont have the perms to mute members")
+
+@unmute.error
+async def unban_error(error, ctx):
+    if isinstance(error, MissingPermissions):
+        await ctx.message.send("You dont have the perms to unmute members")
+
+#exception handling end
 
 client.run(TOKEN)
